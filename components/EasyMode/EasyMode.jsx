d52ae3d7fe6ef03084ecf5ba09dc6e89e1a90d62
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2025-07-08 17:48:57 +0800

    Add tilting effect on the cards

--- components/EasyMode/EasyMode.jsx
+++ components/EasyMode/EasyMode.jsx
@@ -1,4 +1,5 @@
 import "./EasyMode.css"
+import Tilt from 'react-parallax-tilt'
 import {useState, useEffect} from 'react'
 
 export default function EasyMode(){
@@ -7,7 +8,7 @@ export default function EasyMode(){
 
     useEffect(()=>{
         const fetchCharacters = async () => {
-        const response = await fetch('https://rickandmortyapi.com/api/character/1,2,3');
+        const response = await fetch('https://rickandmortyapi.com/api/character/1,2,6');
         const data = await response.json();
 
         const characterCards = data.map(character => ({
@@ -25,10 +26,12 @@ export default function EasyMode(){
     return (
        <div className="cardContainer">
             {cards.map(card => (
-                <div className="card" key={card.id}>
-                    <img className="cardImage" src={card.imageUrl} alt={card.name} />
-                    <div className="cardName"> {card.name} </div>
-                </div>
+               <Tilt>
+                    <div className="card" key={card.id}>
+                        <img className="cardImage" src={card.imageUrl} alt={card.name} />
+                        <div className="cardName"> {card.name} </div>
+                    </div>
+                </Tilt> 
             ))}
         </div> 
     )
