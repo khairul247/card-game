183191b6ca5fbb6fe703cf9be5df26e8695aba1a
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2025-07-08 21:50:35 +0800

    Add shuffling function when card is clicked, use Fisher-Yates algortihm for true randomness

--- components/EasyMode/EasyMode.jsx
+++ components/EasyMode/EasyMode.jsx
@@ -8,7 +8,7 @@ export default function EasyMode(){
 
     useEffect(()=>{
         const fetchCharacters = async () => {
-        const response = await fetch('https://rickandmortyapi.com/api/character/1,2,6');
+        const response = await fetch('https://rickandmortyapi.com/api/character/1,2,3,4,5');
         const data = await response.json();
 
         const characterCards = data.map(character => ({
@@ -17,17 +17,34 @@ export default function EasyMode(){
             imageUrl: character.image
         }))
 
+        console.log(characterCards);
+
         setCards(characterCards);
         };
 
         fetchCharacters();
     },[]);
 
+    function shuffleCards(cards){
+        const shuffled = [...cards];
+
+        for (let i = shuffled.length-1; i > 0; i--){
+            const random = Math.floor(Math.random()*(i+1));
+            [shuffled[i], shuffled[random]] = [shuffled[random], shuffled[i]]
+        }
+
+        return shuffled;
+    }
+
+    function handleCardClick(){
+        setCards(prevCards => shuffleCards(prevCards))
+    }
+
     return (
        <div className="cardContainer">
             {cards.map(card => (
-               <Tilt>
-                    <div className="card" key={card.id}>
+               <Tilt key={card.id}>
+                    <div className="card" key={card.id} onClick={()=>handleCardClick()}>
                         <img className="cardImage" src={card.imageUrl} alt={card.name} />
                         <div className="cardName"> {card.name} </div>
                     </div>
