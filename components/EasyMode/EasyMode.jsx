4c535dea95d8fed28f5817e6e13f5b9a096dc865
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2025-07-08 17:36:58 +0800

    Use API to fetch images for cards

--- components/EasyMode/EasyMode.jsx
+++ components/EasyMode/EasyMode.jsx
@@ -1,13 +1,35 @@
 import "./EasyMode.css"
+import {useState, useEffect} from 'react'
 
 export default function EasyMode(){
+
+    const [cards, setCards] = useState([]);
+
+    useEffect(()=>{
+        const fetchCharacters = async () => {
+        const response = await fetch('https://rickandmortyapi.com/api/character/1,2,3');
+        const data = await response.json();
+
+        const characterCards = data.map(character => ({
+            id: character.id,
+            name: character.name,
+            imageUrl: character.image
+        }))
+
+        setCards(characterCards);
+        };
+
+        fetchCharacters();
+    },[]);
+
     return (
        <div className="cardContainer">
-            <div className="card">
-                <img className="cardImage" src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Morty Smith" />
-            </div>
-            <div className="card">Rick and Morty</div>
-            <div className="card">Rick and Morty</div>
+            {cards.map(card => (
+                <div className="card" key={card.id}>
+                    <img className="cardImage" src={card.imageUrl} alt={card.name} />
+                    <div className="cardName"> {card.name} </div>
+                </div>
+            ))}
         </div> 
     )
 }
